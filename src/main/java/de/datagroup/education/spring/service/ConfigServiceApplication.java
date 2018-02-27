503a999588e362fe7358f033c9da3307b4a9fd95
dg-mihlo <36478908+dg-mihlo@users.noreply.github.com>
2018-02-27 15:13:44 +0100

    daww

--- src/main/java/de/datagroup/education/spring/service/ConfigServiceApplication.java
+++ src/main/java/de/datagroup/education/spring/service/ConfigServiceApplication.java
@@ -3,9 +3,11 @@ package de.datagroup.education.spring.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableConfigServer
 @SpringBootApplication
+@PropertySource("classpath:/src/main/resources/application.properties")
 public class ConfigServiceApplication {
 	
 	public static void main(String[] args) {
